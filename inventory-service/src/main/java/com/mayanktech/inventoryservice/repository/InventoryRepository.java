e7130fedad05eca27d4be067341480d8da5843e6
mayank <mayankyadav1503@gmail.com>
2026-07-27 23:56:33 +0530

    fix(P3): fix typos, update Dockerfile.layered to Java 21, remove unused import

--- inventory-service/src/main/java/com/mayanktech/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/mayanktech/inventoryservice/repository/InventoryRepository.java
@@ -1,15 +1,12 @@
 package com.mayanktech.inventoryservice.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mayanktech.inventoryservice.modal.Inventory;
 
 public interface InventoryRepository extends JpaRepository<Inventory,Long>{
-
-	Optional<Inventory> findBySkuCode(String skuCode);
 
 	List<Inventory> findBySkuCodeIn(List<String> skuCodeList);
 
